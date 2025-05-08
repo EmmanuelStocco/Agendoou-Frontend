b1390bf6ead9f33b69f032cd971d5eb12f7d388b
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2025-05-08 16:21:10 -0300

    feat: add Entrepreneur type on interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -14,7 +14,30 @@ interface User {
     createdAt: string;
     appointmentsAsClient?: Appointment[];
 }
+ 
 
-interface Entrepreneur {
-    slug: string;
+type Entrepreneur = {
+  id: number
+  businessName: string
+  themeColor: string
+  logoUrl: string
+  description: string
+  availableDays: string[]
+  availableHours: string[]
+  services: string[]
+  slug: string
+  user: {
+    name: string
+    email: string
+  }
+}
+
+const weekdayMap: Record<string, number> = {
+  domingo: 0,
+  segunda: 1,
+  terça: 2,
+  quarta: 3,
+  quinta: 4,
+  sexta: 5,
+  sábado: 6,
 }
