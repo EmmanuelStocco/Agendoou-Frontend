0732afd1ab541f63fc9548b54e5193ffbbc800b0
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2025-05-13 11:47:24 -0300

    refactor: interface adjustment

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -24,7 +24,7 @@ type Entrepreneur = {
   description: string
   availableDays: string[]
   availableHours: string[]
-  services: string[]
+  services: any[]
   slug: string
   user: {
     name: string
